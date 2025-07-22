8620675bb28fed62db6dc04b5dd109ffff458997
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-07-23 01:15:36 +0530

    update

--- day01/first.js
+++ day01/first.js
@@ -1,7 +1,45 @@
+
+// CJS: Common JS module system is told for below line of code for importing the second .js to be executed in first.js
+
 require('./second'); // importing second file here
 
-// CJS: Common JS module system is told for above line of code for importing the second .js
 
-sum(3,4); // will not execute beacuse , firstly we have to import the function to be executed .. , sum function private ki trah aya hoga phele then 
+( function(){
+    console.log("heloo ji i am second");
 
-console.log( "hello world");
+    function sum(a,b){
+        console.log(a+b);
+    }
+    sum(3,4); // this will execute the sum function with 3 and 4 as arguments
+})();
+
+
+// IIFE: Immediately Invoked Function Expression concept is used here, which means the function will execute immediately after its definition, means code of second.js will be be executed in first.js with the help of IIFE, so that we can use the function sum in first.js file.
+
+
+
+// sum(3,4);   // will not execute beacuse , firstly we have to import the function to be executed .. , sum function private ki trah aya hoga phele jo ki execute nhi hoga
+
+
+console.log("hello i am first");
+
+// code will be executed in the order because we have imported second.js in first.js, so the code of second.js will be executed first and then the code of first.js will be executed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- day01/second.js
+++ day01/second.js
@@ -3,3 +3,5 @@ console.log("this is arpit ranjan here");
 function sum(a,b){
     console.log(a+b);
 }
+ sum(7,9);
+ 
