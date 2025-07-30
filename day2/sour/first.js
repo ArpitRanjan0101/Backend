a5f9c3b0d6584d2e1dd0746b00f65715e40f7d58
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-07-31 02:58:57 +0530

    cjs and mjs

---
+++ day2/sour/first.js
@@ -0,0 +1,9 @@
+// we will study over here about new way of import and export function 
+
+// we will study here difference between CJS  and MJS
+// CJS is CommonJS and MJS is Module JavaScript
+
+import sum from './second.js';
+
+sum(5, 10);
+console.log("hello arpit this side here ");
