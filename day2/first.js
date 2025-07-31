bfc6e6567cac000199cb43a191dc15c6918c0788
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-07-31 23:04:44 +0530

    update

---
+++ day2/first.js
@@ -0,0 +1,11 @@
+const {sum ,sub}=require("./second.js");
+
+// Below is the synchronous way of importing and using functions like require in Node.js
+
+// require("./third.js");
+// require('./fourth.js");
+// require("./fifth.js");
+
+sum(5, 10);
+sub(10, 5);
+console.log("hello arpit this side here ");
---
+++ day2/sour/first.js
@@ -0,0 +1,11 @@
+// we arw using here the asynchronous way of importing and using functions like import in Node.js
+
+
+
+import sum from "./second.js";
+
+
+
+sum(3,8);
+
+console.log("hello arpit this side here ");
