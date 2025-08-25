0bcb43e959baddc18556c9c6447d4b5c1a617106
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-08-26 00:56:58 +0530

    feat:update

--- day2/sour/today/first.js
+++ day2/sour/today/first.js
@@ -1,10 +1,16 @@
- const sum = require ("./current/sum");
- const sub = require ("./current/sub");
- const mul = require ("./current/mul");
+ const sum = require("./current/sum");
+ const sub = require("./current/sub");
+ const mul = require("./current/mul");
+
+ // here to use the commonjs modules we have to use the commonjs in package.json file so that node will understand that we are using commonjs modules..
+
+
+ // to use the MJS modules we have to use the type module in package.json file so that node will understand that we are using MJS modules..
+
 
  sum(7,8);
  sub(10,5);
- mul(3,4);
+ mul(3,4);      
  // sum 
  // sub
  // mul
