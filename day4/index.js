5e27e205409add38b24598df0298d3e0e0ee3ea7
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-08-31 01:48:42 +0530

    feat : localhost with port number recogization

--- day4/index.js
+++ day4/index.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req ,res)=>{
+    res.end("Hello jii");   // jo bhi server pe visit krega usse hello ji bolkar greeting hoga
+
+});
 
 
 // server create kre ke baad sunega bhi too woo port number 4000 pe sunega
 // jab bhi koi request aayegi to woo ek callback function call karega jisme req and res hoga
 
-// request and response hum waha se dege , jaha se server create kiye hai
+// request and response hum waha se dege , jaha se server create kiye hai  call bac function banakar aisa krte hai hum
 
 server.listen(4000,()=>{
     console.log("Server is running on port 4000");
