3660989e4e35b5ec3e80765638bd4e87e9ffb936
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-08-30 16:39:53 +0530

    feat :update

--- day3/index.js
+++ day3/index.js
@@ -1,12 +1,32 @@
-setTimeout(() => {
+// setTimeout(() => {
+//     console.log("Hello after 2 seconds");
+// }, 10);      // here time is set to 10 milliseconds after which the callback function will be executed 
+
+
+
+// // seTimeout yaha pe bass ek funtion calll hai , uski implementation LIBUV ke aNDAR LIKHE  hue hai jo ki C language me likha hua hai
+
+// // setTimeout ek web API hai jo ki browser ke andar hota hai 
+// // node js me bhi yehi concept hai but node js me ye LIBUV ke andar likha hua hai jo ki C language me likha hua hai
+// // jab hum setTimeout call karte hai to wo ek timer start kar deta hai aur jab wo timer complete ho jata hai to wo callback function ko call kar deta hai
+// // ye callback function event loop ke through call hota hai
+
+
+let a = 10;
+let b = "Hello myself arpit ranjan";
+
+console.log(b);
+
+function sum(a, b) {
+    return a + b;
+}
+
+setTimeout(() =>{  
+       
     console.log("Hello after 2 seconds");
-}, 10);      // here time is set to 10 milliseconds after which the callback function will be executed 
+},3000)
 
-// seTimeout yaha pe bass ek funtion calll hai , uski implementation LIBUV ke aNDAR LIKHE  hue hai jo ki C language me likha hua hai
+// settimeout hume call back function deta hai jo ki event loop ke through call hota hai
 
-// setTimeout ek web API hai jo ki browser ke andar hota hai 
-// node js me bhi yehi concept hai but node js me ye LIBUV ke andar likha hua hai jo ki C language me likha hua hai
-// jab hum setTimeout call karte hai to wo ek timer start kar deta hai aur jab wo timer complete ho jata hai to wo callback function ko call kar deta hai
-// ye callback function event loop ke through call hota hai
-
- 
+console.log(a);
+console.log(sum(10, 20));   
