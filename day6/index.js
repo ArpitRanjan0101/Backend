9e5d3f4b91357b604fde447ba3bdf2b50ac61105
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-01 01:27:30 +0530

    feat : localhost created

--- day6/index.js
+++ day6/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();  // Sever create kiya humne yaha pe , using the express likh kar aur uss app bhi bolte hai  too , usse humne const app me store kar liya 
 
+app.use((req,res) =>{
+    res.send("Hello ji, welcome to our home page");
+})  // yaha pe humne ek callback function pass kiya hai , jisme req and res hai , req means request and res means response
+// jab bhi koi request aayegi to woo is function ko call karega , aur res.send ke through response dega
 
 
 
