eb4452017af2f766c3e95284eab78ff8a0adba6a
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-01 01:19:09 +0530

    feat : listen the port number

--- day6/index.js
+++ day6/index.js
@@ -2,3 +2,6 @@ const express = require('express');
 
 
 const app = express();  // Sever create kiya humne yaha pe , using the express likh kar aur uss app bhi bolte hai  too , usse humne const app me store kar liya 
+app.listen(4000, () =>{
+    console.log("listening at port 4000");
+})    // yaha se listen karna start kar dega server 4000 port pe
