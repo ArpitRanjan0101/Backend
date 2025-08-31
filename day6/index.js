48269ba8c4df63935b10cbed3c410abb647b959e
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-01 01:20:11 +0530

    feat : update

--- day6/index.js
+++ day6/index.js
@@ -4,4 +4,4 @@ const express = require('express');
 const app = express();  // Sever create kiya humne yaha pe , using the express likh kar aur uss app bhi bolte hai  too , usse humne const app me store kar liya 
 app.listen(4000, () =>{
     console.log("listening at port 4000");
-})    // yaha se listen karna start kar dega server 4000 port pe
+})    // yaha se listen karna start kar dega server 4000 port pe likin ess time koe response nhi dega
