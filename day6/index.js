b61a513426d6c8c121045c3e42d526e7001f7f07
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-01 01:22:05 +0530

    feat : update

--- day6/index.js
+++ day6/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 
 const app = express();  // Sever create kiya humne yaha pe , using the express likh kar aur uss app bhi bolte hai  too , usse humne const app me store kar liya 
+
+
+
+
+
 app.listen(4000, () =>{
     console.log("listening at port 4000");
-})    // yaha se listen karna start kar dega server 4000 port pe likin ess time koe response nhi dega
+})    // yaha se listen karna start kar dega server 4000 port pe likin ess time koe response nhi dega  , to reply dene keliye hum app.use ka use karenge
+
+// app.use() ka use karte hai hum , jab bhi koi request aayegi to woo app.use ke andar jo function hoga usse call karega
+// app.use ke andar ek callback function pass karte hai jisme req and res hota hai
+// req means request and res means response
