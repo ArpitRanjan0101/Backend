c343f0b9a92c230003735e33ac2ab1e5d264e4bb
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-01 01:16:28 +0530

    feat : created server

---
+++ day6/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+
+
+const app = express();  // Sever create kiya humne yaha pe , using the express likh kar aur uss app bhi bolte hai  too , usse humne const app me store kar liya 
