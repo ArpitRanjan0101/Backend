b1b74ee91089e5768694cbfb697c672ef44e1db4
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-01 23:32:40 +0530

    feat : update

--- day6/index.js
+++ day6/index.js
@@ -3,17 +3,31 @@ const express = require('express');
 
 const app = express();  // Sever create kiya humne yaha pe , using the express likh kar aur uss app bhi bolte hai  too , usse humne const app me store kar liya , yeah ek actually instant hai , jo btatata hai ki humne server create kiya hai
 
-app.use((req,res) =>{
-    res.send("Hello ji, welcome to our home page");
-})  // yaha pe humne ek callback function pass kiya hai , jisme req and res hai , req means request and res means response
+
+
+// app.use((req,res) =>{
+//     res.send("Hello ji, welcome to our home page");
+// }) 
+
+// yaha pe humne ek callback function pass kiya hai , jisme req and res hai , req means request and res means response
 // jab bhi koi request aayegi to woo is function ko call karega , aur res.send ke through response dega
+
+// we can also send the data in json format niche diye hue code me likha hua hai , just check it out
+
+app.use((req,res) =>{
+    res.send({name : "Nexus", age : 22, city : "Bangalore"});
+}) 
+
+// yaha pe humne ek callback function pass kiya hai , jisme req and res hai , req means request and res means response
+// jab bhi koi request aayegi to woo is function ko call karega , aur res.json ke through response dega in json format
 
 
  
 
 app.listen(4000, () =>{
     console.log("listening at port 4000");
-})    // yaha se listen karna start kar dega server 4000 port pe likin ess time koe response nhi dega  , to reply dene keliye hum app.use ka use karenge
+})    // yaha se listen karna start kar dega server 4000 port pe likin ess time koe response nhi dega  , to reply dene keliye hum app.use ka use karenge , jo ki upar likha hua hai
+// jab bhi koi request aayegi to woo app.use ke andar jo function hoga usse call karega
 
 // app.use() ka use karte hai hum , jab bhi koi request aayegi to woo app.use ke andar jo function hoga usse call karega
 // app.use ke andar ek callback function pass karte hai jisme req and res hota hai
