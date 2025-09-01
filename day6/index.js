9fd0aa8fefc64d63f47a3d4d17a37677b00afb8b
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-02 00:21:32 +0530

    feat : update

--- day6/index.js
+++ day6/index.js
@@ -14,12 +14,28 @@ const app = express();  // Sever create kiya humne yaha pe , using the express l
 
 // we can also send the data in json format niche diye hue code me likha hua hai , just check it out
 
-app.use((req,res) =>{
-    res.send({name : "Nexus", age : 29, city : "Bangalore"});
-}) 
+// app.use((req,res) =>{
+//     res.send({name : "Nexus", age : 29, city : "Bangalore"});
+// }) 
 
 // yaha pe humne ek callback function pass kiya hai , jisme req and res hai , req means request and res means response
 // jab bhi koi request aayegi to woo is function ko call karega , aur res.json ke through response dega in json format
+
+
+app.use("/about",(req,res) =>{
+    res.send({name : "arpit", age : 12, city : "california"});
+})
+
+app.use("/contact",(req,res) =>{
+    res.send(" hello everyone , this side arpit ranjan");
+})
+
+// yaha pe humne 2 alag alag route banaye hai , about and contact , jab bhi koi request aayegi to woo is function ko call karega , aur res.send ke through response dega
+
+// agar hum chahte hai ki jab bhi koi about ke route pe request bheje to woo about ka hi response de , aur contact ke route pe request bheje to woo contact ka hi response de , to hum app.use ke andar pehla argument me route ka naam de denge , jese upar diye hue code me likha hua hai
+
+
+
 
 
  
