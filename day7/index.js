d1995e727cb8c14bc97e1c979851203549ec29bd
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-02 22:00:48 +0530

    feat :update

--- day7/index.js
+++ day7/index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 
 
-app.use("/user",(req , res)=>{
+// app.use("/user",(req , res)=>{
+
+//     res.send("hello guys , Heer here this side");
+// })
+
+
+// we use here GET METHOD to fetch the data from the server
+
+
+app.get("/user",(req , res)=>{
 
     res.send("hello guys , Heer here this side");
 })
+
 
 app.listen(4000 ,()=>{
     console.log("listening at port 4000");
