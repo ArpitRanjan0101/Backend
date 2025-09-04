6d05e49ba44633ebf8dfcacf1aa6265ac30114dc
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-04 18:30:57 +0530

    feat : update

--- day7/index.js
+++ day7/index.js
@@ -13,7 +13,21 @@ const app = express();
 
 app.get("/user",(req , res)=>{
 
+    console.log(req); // yah pe bhut sare request jate hai request ko print krne pe
     res.send("hello guys , Heer here this side"); // we can also print json data here using get method
+})
+
+
+// below is the post mentod to send the data to the server
+// post method is used to send the data to the server
+// post method is used to create the data on the server
+// post method is used to update the data on the server
+// post method is used to delete the data on the server
+// post method is used to fetch the data from the server
+
+app.post("/user",(req , res)=>{
+    console.log("data saved successfully");
+    res.send("Data saved successfully"); // we can also print json data here using get method
 })
 
 
