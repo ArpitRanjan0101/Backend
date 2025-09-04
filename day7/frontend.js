3f1c920e0f9273a6185ffa9c0940cdab5fb653b7
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-04 22:10:59 +0530

    feat : update in push , patch , post and get

--- day7/frontend.js
+++ day7/frontend.js
@@ -3,7 +3,11 @@
 // default method get hi hota hai
 // means by default fetch get method hi hota hai
 // means agar hum fetch me method define nhi krte hai to yeah get method hi leta hai
-fetch("https://fktuygjbd.com")
+
+// we need not to define to much things like that of Post method , sinc eit get already understood that this get method and fetching the data from the backend 
+
+// Default method is GET method
+const response2  = await fetch("https://fktuygjbd.com");
 
 
 //below is the post method to send the data to the server
@@ -24,4 +28,37 @@ const response = await fetch("https://fktuygjbd.com",{
         age : 21
     })
 });
+
+
+// Patch and Put method bhi same hi hota hai ,  Post ke jaisa hi work krta hai
+// difference is that , put me pura data update krta hai , aur patch me kuch part hi update krta hai
+// put me agar hum kuch field nhi bhejte hai to wo field null ho jati hai
+// patch me agar hum kuch field nhi bhejte hai to wo field waise hi rehti hai
+
+// Patch method
+
+// const response3 = await fetch("https://fktuygjbd.com",{
+//     method : "PATCH",
+//     headers : {
+//         "Content-Type" : "application/json"
+//     },
+//     body : JSON.stringify({
+//         name : "Heer",
+//         age : 21
+//     })
+// });
+
+
+// Put method
+
+// const response4 = await fetch("https://fktuygjbd.com",{
+//     method : "Put",
+//     headers : {
+//         "Content-Type" : "application/json"
+//     },
+//     body : JSON.stringify({
+//         name : "Heer",
+//         age : 21
+//     })
+// });
  
