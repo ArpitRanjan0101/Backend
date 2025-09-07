99c6ff9926e54e203f725c139be71a4079cec6f1
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-08 02:26:57 +0530

    FEAT : server setup

--- day8/first.js
+++ day8/first.js
@@ -1,1 +1,9 @@
  // WE WILL STUDY HERE ABOUT THE MIDDLEWARE
+
+ const express = require("express");
+ const app = express();
+
+
+ app.listen(3000,()=>{
+    console.log("Listening at port 3000");
+ })
