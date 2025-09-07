555643977a4df3e772f6c9113dd9cb39c52bee71
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-08 02:56:41 +0530

    feat : update

--- day8/first.js
+++ day8/first.js
@@ -3,6 +3,21 @@
  const express = require("express");
  const app = express();
 
+  app.use("/user",(req,res)=>{
+
+   res.send("Heelo ji")
+
+  })
+
+
+
+
+
+
+
+
+
+
 
  app.listen(3000,()=>{
     console.log("Listening at port 3000");
