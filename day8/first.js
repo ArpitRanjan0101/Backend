6f7582384b1b142a41b0b2dc7d1ad5c4172d567b
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-08 15:05:18 +0530

    feat : update

--- day8/first.js
+++ day8/first.js
@@ -3,14 +3,19 @@
  const express = require("express");
  const app = express();
 
-  app.use("/user",(req,res)=>{
+  app.use("/user",(req,res,next)=>{
 
    console.log("first");
-   res.send("Heelo ji");
+   // res.send("Heelo ji");
    console.log("First First");
    // res.send("Mein Mast hu"); // two response possible nhi hai , ek single request pe
-
-  })
+   next();  // yeah next niche wale code ka reference ko store krke rakh rha hai , aur call kr rha hai next ko hi , jaise hi call kiya then niche wala code execute hone lgega
+ },
+  (req,res)=>{
+   console.log("Second");
+   res.send("I am Second");
+  }
+)
 
 
 
