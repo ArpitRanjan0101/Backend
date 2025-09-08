4b572ae85b9fc32c46b64f7c51bace1357744769
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-08 21:44:27 +0530

    Feat : server setup

---
+++ day9/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+// CRUD : Create Read update Delete , we will study here
+
+
+
+app.listen(3000,()=>{
+    console.log("Listening at the port 3000");
+})
