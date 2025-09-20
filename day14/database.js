795e750b641e37d88d7ee4e4c4dc681ca803e3e4
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-20 14:34:35 +0530

    feat : express and database setup done

---
+++ day14/database.js
@@ -0,0 +1,4 @@
+const url ="mongodb+srv://arpitvijans1234:<db_password>@clustertest.itezwo1.mongodb.net/";  
+
+// above link , we have mentioned to connect with database server of the mongodb atlas , which is actually a cloud service to store the data of our application
+// but we have not mentioned the database name , so we will add that in the below code
