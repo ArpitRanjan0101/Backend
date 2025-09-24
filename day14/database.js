d1bbd8b621d439325173806b82c40c46e861f936
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-24 22:40:55 +0530

    feat : server setup

--- day14/database.js
+++ day14/database.js
@@ -3,4 +3,4 @@ const url ="mongodb+srv://arpitvijans1234:<db_password>@clustertest.itezwo1.mong
 // above link , we have mentioned to connect with database server of the mongodb atlas , which is actually a cloud service to store the data of our application
 // but we have not mentioned the database name , so we will add that in the below code
 // also we will connect through mongoose
-// we will see the full working 
+// we will see the full working   
