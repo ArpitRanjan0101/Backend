afc7b91fab197a3ff95db22d06b3a90f9970d12a
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-26 16:33:29 +0530

    feat : model finished

--- day15/database.js
+++ day15/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function main() {   // TO connect with the mongodb , we are writing this function
-    await mongoose.connect('mongodb+srv://arpitvijans1234:arpitvijans11111@clustertest.itezwo1.mongodb.net/');
+    await mongoose.connect('mongodb+srv://arpitvijans1234:arpitvijans11111@clustertest.itezwo1.mongodb.net/BookStore');   // URl link ke last me humne database ka name likha hai , woo automatically create ho jayega agar wo exist nhi krta hoga to , simply , we need not create database manually
 
     // all code will be writing over here
 
@@ -11,6 +11,9 @@ async function main() {   // TO connect with the mongodb , we are writing this f
         city: String,
         gender: String   
     })
+
+    // Model ko create krna === Collection ko create krna(table ko create krna)
+   const User = mongoose.model('User', userSchema);   // User is the name of the collection , aur uss user collection ka schema upar likha hua hai  , esse ko model create krna bolte hai aur collection create krna bhi bolte hai 
 }
 
 
