6fbde2c756639106e4a27d00f0b13ccf3f56ef4a
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-26 17:10:51 +0530

    feat : collection , documents, db done

--- day15/database.js
+++ day15/database.js
@@ -13,7 +13,22 @@ async function main() {   // TO connect with the mongodb , we are writing this f
     })
 
     // Model ko create krna === Collection ko create krna(table ko create krna)
+// Simply bol skte hai ki Class ko create kiya hai 
+
    const User = mongoose.model('User', userSchema);   // User is the name of the collection , aur uss user collection ka schema upar likha hua hai  , esse ko model create krna bolte hai aur collection create krna bhi bolte hai 
+
+   // if we want to create the object of the class , we just use the new keyword aur esko Document ko create kiya hai bol skte hai
+   const user1 = new User({name: "Arpit", age: 21, city:"Hazaribagh", gender:"Male"});
+
+   await user1.save();   // save() function is used to save the object in the database
+
+   await User.create({name: "Ankit", age: 22, city:"Ranchi",});   // create() function is used to create the object and save it in the database directly
+
+
+   // if we want to create the multiple objects at a time , we can use insertMany() function
+    await User.insertMany([{name:"Ankit", age: 22, city:"Ranchi", gender:"Male"}])
+
+
 }
 
 
