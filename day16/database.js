b5d9209ac70ca88d1b354a867c57ec3ed6d51d71
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-09-29 01:47:31 +0530

    feat :update

---
+++ day16/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+async function main() {
+    await mongooose.connect('mongodb+srv:arpitvijans1234:arpitvijans11111@clustertest.itezwo1.mongodb.net/BookStore');
+}
+
+
+module.exports = main;  
---
+++ day16/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const main = require('./database');
+const User =require("./Models/users")
+
+app.use(express.json());
+
+
+// CRUD : Create Read Update Delete
+
+main()
+.then(async()=>{
+    console.log("Connected to DB");
+    app.listen(3000,()=>{
+        console.log("Listening at port 3000");
+    })
+})
+
+.catch((err)=>console.log(err));
