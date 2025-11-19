9911e8aa68b5b349dc24c73844bf5f5494f3989d
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-11-19 22:49:14 +0530

    Refactor user schema to include detailed fields for first name, last name, email, password, and photo

--- day17/users.js
+++ day17/users.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
+
+// We have made user Schema here , with the help of which , we can make api call , this schema design will be shown over the database 
+
 const userSchema = new Schema({
-    name: String,
-    age: Number,
-    city : String,
-    gender: String
+   firstName:{
+    type:String
+   },
+   lastName:{
+    type:String 
+   }, 
+   age:{
+        type:Number
+   },
+   gender:{
+        type:String
+   },
+   emailId:{
+        type:String
+   },
+   password:{
+        type:String
+   },
+   photo:{
+        type:String  // type isislye string kyuki hum url store karenge , photo kahi aur hi store hogi
+   }
 });
 
 const User = mongoose.model("user", userSchema);
 
-module.exports = User;
+module.exports = User; 
