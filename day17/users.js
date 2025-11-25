bb659d9e1823a519bc326339b2b9ffc0c38e254f
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-11-26 01:42:48 +0530

    Refactor user schema: add unique constraint and length validations for firstName

--- day17/users.js
+++ day17/users.js
@@ -7,7 +7,11 @@ const {Schema} = mongoose;
 const userSchema = new Schema({
    firstName:{
     type:String,
-    required:true  // this is actually a validation we are applying here to validate that first name is required field , agar jab tak hum required :true nahi karenge tab tak hum user create  nhi kar payenge bina first name ke bhi
+    required:true,  // this is actually a validation we are applying here to validate that first name is required field , agar jab tak hum required :true nahi karenge tab tak hum user create  nhi kar payenge bina first name ke bhi
+    unique:true, // ye bhi ek validation hai jiska matlab hai ki koi bhi do user same first name ke sath register nahi kar payenge
+     minlength:3,  // this tell us we should not go below this length to name the user
+     maxlength:30
+
    },
    lastName:{
     type:String 
@@ -28,7 +32,8 @@ const userSchema = new Schema({
         type:String
    },
    photo:{
-        type:String  // type isislye string kyuki hum url store karenge , photo kahi aur hi store hogi
+        type:String, // type isislye string kyuki hum url store karenge , photo kahi aur hi store hogi
+        default:"this is the default photo"
    }
 });
 
