1a94d1086756c627150d71798ced8b8cf4d7c83c
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-11-25 20:39:53 +0530

    connected with  moongoose and done with error

--- day17/index.js
+++ day17/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const main = require("./database");
-const User = require("./Models/users");
+const User = require("./users");
 
 app.use(express.json());
 
 
-main().then(()=>{
+main()
+.then(async()=>{                      // here we did mnistake of not using then and catch with main function which returns a promise
     console.log("Connected to DB")
     app.listen(3000,()=>{
         console.log("Listening at port 3000");
-    });
+    })
 })
-.catch((err)=>console.log(err));
+.catch((err)=>console.log(err));  
