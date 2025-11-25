a666fe25343111810d795ade4c4eeafc96d912ee
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-11-25 20:06:10 +0530

    update

--- day17/index.js
+++ day17/index.js
@@ -4,3 +4,12 @@ const main = require("./database");
 const User = require("./Models/users");
 
 app.use(express.json());
+
+
+main().then(()=>{
+    console.log("Connected to DB")
+    app.listen(3000,()=>{
+        console.log("Listening at port 3000");
+    });
+})
+.catch((err)=>console.log(err));
