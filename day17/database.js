889d1e597a809fd5628ff044883b1c7f6ed772c7
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-11-27 23:49:48 +0530

    Refactor database connection comment for clarity and add initial README file

--- day17/database.js
+++ day17/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// moongose is a library which helps us to connect to mongodb database from our nodejs application
 async function main() {
     await mongoose.connect("mongodb+srv://arpitvijans1234:arpitvijans11111@clustertest.itezwo1.mongodb.net/Instagram") // in the previous database we had created the database with Bookstore name but now in the new connection  we are creating the database with Aadhar name
 }
