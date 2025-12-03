57d9a571f723aced847ad8a9f623f71721eb6420
Arpit Ranjan <153373289+ArpitRanjan0101@users.noreply.github.com>
2025-12-04 02:05:54 +0530

    Refactor comments for clarity in database and user model files

--- day18/Models/users.js
+++ day18/Models/users.js
@@ -1,1 +1,2 @@
 // inn this file the scheme design for the user collection  will be done here ,simply the user model will be created here
+// schema design is done here , through we can see the api structure 
