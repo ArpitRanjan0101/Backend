a6761b2b1890b538c69f3f8d45a36dd3663b9f3d
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2026-03-24 20:48:16 +0530

    feat : update in code of mcp server

--- webrtc/index.js
+++ webrtc/index.js
@@ -1,3 +1,9 @@
 // here in this file we will study about the webrtc module and mcp server
 // webrtc module is used for real time communication between two peers
 // mcp server is used for media control protocol server
+const express = require('express');
+const app = express();  
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
